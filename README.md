fef23f0b1eb9064162b05e3120fb989f58374f71
lakshyabatman <lakshya.khera@gmail.com>
2019-05-06 14:10:08 +0530

    now you can add filename.txt as argv

---
+++ README.md
@@ -0,0 +1,8 @@
+#Huffman-PY
+
+This mini-project is for the Algorithms and Problem Solving 
+
+<h3>Hoe to Run this File</h3>
+<ul>
+<li>Fork this repo</li>
+<li><code>python3 huffman.py "Your text or file name"</code></li>
--- huffman.py
+++ huffman.py
@@ -1,4 +1,6 @@
 import sys
+
+
 class Node():
    def __init__(self,letter):
         self.letter=letter
@@ -19,42 +21,62 @@ def huffman_traversal(NodeObj,myarr):
             huffman_traversal(NodeObj.right,myarr)
     return myarr
 
-str=sys.argv[1]
-
-print(str)
-all_freq = {} 
-for i in str: 
-	if i in all_freq: 
-		all_freq[i] += 1
-	else: 
-		all_freq[i] = 1
-all_freq.pop(' ')
-count=len("".join(str.split(" ")))
-charObj=[]
-for key in all_freq:
-    temp= Node(letter=key)
-    temp.count=all_freq[key]
-    temp.probability=(temp.count/count)
-    charObj.append(temp)
 
 
-while(len(charObj)>1):
-    charObj.sort(key=lambda x: x.probability)
-    smallest=charObj[0]
-    second_smallest=charObj[1]
-    temp_node= Node(smallest.letter+second_smallest.letter)
-    temp_node.probability=smallest.probability+second_smallest.probability
-    temp_node.left=smallest
-    temp_node.right=second_smallest
-    charObj.append(temp_node)
-    charObj.remove(smallest)
-    charObj.remove(second_smallest)
-letter_code={}
-letter_code=huffman_traversal(charObj[0],myarr=letter_code)
-enrypted_str=""
-for i in str:
-    if(i==" "):
-        enrypted_str+="  "
+def stringToObjList(str):
+    all_freq = {} 
+    for i in str: 
+        if i in all_freq: 
+            all_freq[i] += 1
+        else: 
+            all_freq[i] = 1
+    try:
+        all_freq.pop(' ')
+    except:
+        print(all_freq)
+    count=len("".join(str.split(" ")))
+    charObj=[]
+    for key in all_freq:
+        temp= Node(letter=key)
+        temp.count=all_freq[key]
+        temp.probability=(temp.count/count)
+        charObj.append(temp)
+    return charObj
+
+def ObjListToTree(charObj):
+    while(len(charObj)>1):
+        charObj.sort(key=lambda x: x.probability)
+        smallest=charObj[0]
+        second_smallest=charObj[1]
+        temp_node= Node(smallest.letter+second_smallest.letter)
+        temp_node.probability=smallest.probability+second_smallest.probability
+        temp_node.left=smallest
+        temp_node.right=second_smallest
+        charObj.append(temp_node)
+        charObj.remove(smallest)
+        charObj.remove(second_smallest)
+    return charObj
+
+def checkStrOrFile(str):
+    print(str[-4:])
+    if(str[-4:]==".txt"):
+         f=open(str,"r")
+         str=f.read()
+         return str
     else:
-        enrypted_str+=" "+letter_code[i]
-print(enrypted_str)
+        return str
+
+if __name__ == "__main__": 
+    str=sys.argv[1]
+    str=checkStrOrFile(str)
+    charObj=stringToObjList(str)
+    charObj=ObjListToTree(charObj)
+    letter_code={}
+    letter_code=huffman_traversal(charObj[0],myarr=letter_code)
+    enrypted_str=""
+    for i in str:
+        if(i==" "):
+            enrypted_str+="  "
+        else:
+            enrypted_str+=" "+letter_code[i]
+    print(enrypted_str)
